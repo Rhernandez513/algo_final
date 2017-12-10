65721f856d428acc0a1524ae56dc4ae9dff47177
Robert David Hernandez <rhernandez513@gmail.com>
2017-12-10 14:48:57 -0600

    add GetNeighborTest

--- src/test/java/io/roberthernandez/GraphTest.java
+++ src/test/java/io/roberthernandez/GraphTest.java
@@ -5,6 +5,8 @@ import io.roberthernandez.Graph;
 import org.junit.Test;
 import org.junit.Assert;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 
@@ -14,6 +16,17 @@ public class GraphTest {
         int vertices = 2;
         Graph graph = new Graph(2);
         assertEquals(vertices, graph.getNumVertices());
+    }
+
+    @Test
+    public void GetNeighborTest() {
+        int vertices = 2;
+        Graph graph = new Graph(vertices);
+        graph.addEdge(1, 2);
+        ArrayList<Integer> neighbours = graph.getNeighbours(1);
+        String value = neighbours.get(0).toString();
+        int valueAsInt = Integer.parseInt(value);
+        assertEquals(valueAsInt, 2);
     }
 
 //    @Test
